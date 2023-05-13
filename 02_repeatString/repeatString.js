28088a7b21455005821135cc6c7ca0f6f582023b
shenshimmd <60430653+shenshimmd@users.noreply.github.com>
2023-05-13 20:36:32 +1000

    add loop and if condition
    if condition to check the loop repeat times is >=0 or return error

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function(word, times) {
+    if (times >= 0){
+        let string = "";
+        for(let i = 0; i < times; i++){
+            string += word;
+        }
+        return string;
+    }
+    else {
+        return "ERROR";
+    }
+
 
 };
 
