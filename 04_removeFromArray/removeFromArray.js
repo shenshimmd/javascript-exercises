0f73d2c8ca0d9005926f79788ace95008020afb4
shenshimmd <60430653+shenshimmd@users.noreply.github.com>
2023-05-18 16:44:55 +1000

    add function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    theArgs.forEach(arg => {
+        let index = array.indexOf(arg);
+        if(index !== -1) {
+            array.splice(index, 1)
+        }
+    });
+    return array;
 };
 
 // Do not edit below this line
