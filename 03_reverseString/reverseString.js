9648671dcb6de82d7fb967d30e919d89fd16321d
shenshimmd <60430653+shenshimmd@users.noreply.github.com>
2023-05-18 16:44:46 +1000

    add function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let reverse = word.split("");
+    let reversed = "";
+    for(let i = 0; i <reverse.length; i++){
+        reversed = reversed + reverse[(reverse.length - i - 1)];
+    }
+    return reversed;
 };
-
+//or return word.split("").reverse().join("");
 // Do not edit below this line
 module.exports = reverseString;
